df7ab73478bc87b6171c225903c268af47aa752a
turing <(none)>
2024-10-21 20:49:24 +0530

    FIX: Fixed element locators

--- src/celery_app/grainger_scraper.py
+++ src/celery_app/grainger_scraper.py
@@ -1,5 +1,6 @@
 from playwright.async_api import async_playwright, Route, Request
 from random import randint
+import traceback
 
 async def save_resources(route: Route, request: Request):
     if request.resource_type in ["image", "media", "font"]:
@@ -9,6 +10,9 @@ async def save_resources(route: Route, request: Request):
     
 
 async def run_scrape(url, zipcode):
+    
+    pid = url.split('-')[-1].split('?')[0]
+
     async with async_playwright() as p:
         try:
             browser = await p.firefox.launch(headless=False, proxy={
@@ -25,27 +29,33 @@ async def run_scrape(url, zipcode):
 
             # await page.route("*/**", save_resources)
 
-            res = await page.goto(url)
+            res = await page.goto(url, wait_until="domcontentloaded")
 
             if res.status == 404:
                 return await page.content(), "", 404
 
-            await page.get_by_test_id("rta-options-list").locator("span").nth(2).click()
-            await page.get_by_text("Ship", exact=True).click()
+            try:
+                page.wait_for_selector("button:has-text('Change')",timeout=10000)
+                page.locator("button:has-text('Change')").click()
+            except Exception as ex:
+                pass
 
-            await page.locator("label").filter(has_text="Ship").click()
-            await page.get_by_test_id("rta-zip-input").click()
-            await page.get_by_test_id("rta-zip-input").fill(str(zipcode))
-            await page.get_by_test_id("button-zip-code-form").click()
+            await page.locator("label").filter(has_text="Ship").first.click()            
+            await page.wait_for_selector('input[name="zipCode"]')
+            await page.locator('input[name="zipCode"]').first.fill(str(zipcode))
+            await page.locator('button[aria-label="Save ZIP Code"]').first.click()
             main_page_content = await page.content()
 
-            await page.get_by_test_id("button-add-to-cart-2082676").click()
-            await page.get_by_text("View Cart").click()
+            await page.locator(f'form[data-testid="add-to-cart-form-{pid}"] button').click()
+            await page.wait_for_selector('button.add-to-cart__view-cart')
+            await page.locator('button.add-to-cart__view-cart').first.click()
+
             await page.get_by_text("Order Summary").nth(1).hover()
             shipping_content = await page.content()
 
             return main_page_content, shipping_content, 200
         except Exception as ex:
+            traceback.print_exc()
             await page.screenshot(path=f"htmls/{randint(0, 10000)}.jpeg", type="jpeg", full_page=True)
         
     
