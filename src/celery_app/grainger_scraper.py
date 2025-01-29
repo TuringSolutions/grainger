698ee99c14e22a33b0710ee34e519f64dbcb513e
DhruvilGadhvi <dhruvilgadhvi1998@gmail.com>
2025-01-29 15:55:20 +0530

    updated playwright browser to chrome

--- src/celery_app/grainger_scraper.py
+++ src/celery_app/grainger_scraper.py
@@ -2,6 +2,11 @@ from playwright.async_api import async_playwright, Route, Request
 from random import randint
 import traceback
 
+def load_urls_from_file(file_path):
+    with open(file_path, 'r') as file:
+        urls = [line.strip() for line in file.readlines() if line.strip()]
+    return urls
+
 def check_product_stock(content):
     if any([x.lower() in content.lower() for x in [
         "Sorry we're having difficulty finding a match for the term(s) you've entered. You can also search for products using other keywords and item numbers.",
@@ -15,15 +20,17 @@ async def save_resources(route: Route, request: Request):
     else:
         await route.continue_()
 
+
 async def run_scrape(url, zipcode):
     pid = url.split('?')[0].split('/')[-1].split('-')[-1]
     async with async_playwright() as p:
         try:
-            browser = await p.firefox.launch(headless=False, proxy={
-                "server": "geo.iproyal.com:12321",
+            browser = await p.chromium.launch(headless=False,proxy={
+                "server": "http://geo.iproyal.com:12321",
                 "username": "RAD5VCH0WnT6glQG",
                 "password": "uJUnzLRMv5c5Ap0Z_country-us"
             })
+
             ctx = await browser.new_context()
             await ctx.add_init_script(
                 "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
@@ -44,14 +51,14 @@ async def run_scrape(url, zipcode):
             except:
                 pass
 
-            if check_product_stock(first_content):
-                return first_content, "", 200
-
-            try:#Discontinued
+            try:
                 await page.wait_for_selector("div[data-testid='notification-discontinued']", timeout=10000)
                 return first_content, "", 200
             except:
                 pass
+
+            if check_product_stock(first_content):
+                return first_content, "", 200
 
             try:
                 await page.wait_for_selector("button:has-text('Change')", timeout=10000)
@@ -72,6 +79,7 @@ async def run_scrape(url, zipcode):
             await page.locator(f'form[data-testid="add-to-cart-form-{pid}"] button').click()
 
             try:#case 1 popup url:https://www.grainger.com/product/15F493
+                """updated dialog box popups """
                 await page.wait_for_selector('button[name="accept"]',timeout=5000)
                 await page.locator('button[name="accept"]').click()
             except Exception as ex:
@@ -90,6 +98,7 @@ async def run_scrape(url, zipcode):
             except:
                 pass
 
+            """updated view cart section"""
             await page.wait_for_selector('button:has-text("Continue Shopping")')
             await page.locator('button:has-text("Continue Shopping") >> .. >> a:has-text("View Cart")').click()
 
